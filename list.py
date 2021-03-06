fc7f68d910f7fe28e224039c88427120c1eba282
sreeganga <78845970+sreeganga@users.noreply.github.com>
2021-03-06 21:05:08 +0530

    Delete list.py

--- list.py
+++
@@ -1,8 +0,0 @@
-number=int(input("Enter Total number of elements in list : "))
-lists=[]
-for i in range(number):
-    value=int(input("Enter a number :"))
-    lists.append(value)
-    
-result = [each for each in lists if each>0]
-print(result)
