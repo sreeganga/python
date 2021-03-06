b25a7104ac06e3c198a539b385054a72a102868d
sreeganga <78845970+sreeganga@users.noreply.github.com>
2021-03-06 21:12:37 +0530

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,6 @@
+area_of_a_rectangle = lambda l,b : l*b
+area_of_a_square=lambda a: a*a
+area_of_a_triangle=lambda l,b: (1/2)*l*b
+print("Area of rectangle is:", area_of_a_rectangle(3,4))
+print("Area of square is:", area_of_a_square(5))
+print("Area of triangle is:", area_of_a_triangle(6,7))
